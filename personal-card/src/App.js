b55087cfb466e71a244c7847b8d5a0c86db5ff33
JoacoMaurtua <joacomaurtua@gmail.com>
2021-06-10 23:39:09 -0500

    second way

--- personal-card/src/App.js
+++ personal-card/src/App.js
@@ -2,15 +2,37 @@ import './App.css';
 import './components/PersonCard';
 import PersonCard from './components/PersonCard';
 
-const App = () =>{
+const dataBase = [
+  { firstName: 'Jane', lastName: 'Doe', age: 45, hairColor: 'Black' },
+  { firstName: 'John', lastName: 'Smith', age: 88, hairColor: 'Brown' },
+  { firstName: 'Millard', lastName: 'Fillmore', age: 50, hairColor: 'Brown' },
+  { firstName: 'Maria', lastName: 'Smith', age: 62, hairColor: 'Brown' },
+];
+
+
+const App = () => {
+  const db = dataBase.map((items, index) => {
+    const { firstName, lastName, age, hairColor } = items; //extraer las propiedades
+    return (
+      <PersonCard
+        firstName={firstName}
+        lastName={lastName}
+        age={age}
+        hairColor={hairColor}
+        key={index}
+      />
+    );
+  });
+
   return (
     <div className="App">
-      <PersonCard firstName = {"Jane"} lastName = {"Doe"} age = {45} hairColor = {"Black"}/>
+      {db}
+      {/* <PersonCard firstName = {"Jane"} lastName = {"Doe"} age = {45} hairColor = {"Black"}/>
       <PersonCard firstName = {"John"} lastName = {"Smith"} age = {88} hairColor = {"Brown"}/>
       <PersonCard firstName = {"Millard"} lastName = {"Fillmore"} age = {50} hairColor = {"Brown"}/>
-      <PersonCard firstName = {"Maria"} lastName = {"Smith"} age = {62} hairColor = {"Brown"}/>  
+      <PersonCard firstName = {"Maria"} lastName = {"Smith"} age = {62} hairColor = {"Brown"}/>  */}
     </div>
   );
-}
+};
 
 export default App;
--- personal-card/src/components/PersonCard.js
+++ personal-card/src/components/PersonCard.js
@@ -1,19 +1,19 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-class PersonCard extends Component{
-  render(){
-    const {firstName, lastName, age, hairColor} = this.props;
-    return(
-      <div className="container">
-        <h2>
-          {lastName},{firstName}
-        </h2>
-        <p>Age:{age}</p>
-        <p>Hair Color:{hairColor}</p>
-        <br/>
-      </div>
-    );
-  }
+const PersonCard =(props)=> {
+ 
+  const {firstName, lastName, age, hairColor} = props;
+  return(
+    <div className="container">
+      <h2>
+        {lastName},{firstName}
+      </h2>
+      <p>Age:{age}</p>
+      <p>Hair Color:{hairColor}</p>
+      <br/>
+    </div>
+  );
+  
 }
 
 export default PersonCard;

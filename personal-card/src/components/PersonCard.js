d136cfdefc52c410bb589c3cc6bd0cad24a565c7
JoacoMaurtua <joacomaurtua@gmail.com>
2021-06-10 22:44:39 -0500

    first react

---
+++ personal-card/src/components/PersonCard.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+class PersonCard extends Component{
+  render(){
+    const {firstName, lastName, age, hairColor} = this.props;
+    return(
+      <div className="container">
+        <h2>
+          {lastName},{firstName}
+        </h2>
+        <p>Age:{age}</p>
+        <p>Hair Color:{hairColor}</p>
+        <br/>
+      </div>
+    );
+  }
+}
+
+export default PersonCard;
